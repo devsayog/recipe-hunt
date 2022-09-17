75a49c971267fc44da5357d26331182b1ddcbdd5
devsayog <devsayog@gmail.com>
2022-09-17 14:59:24 +0545

    add area link in navbar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -53,6 +53,32 @@ const Header = () => {
             </Link>
           </li>
           <li>
+            <Link href="/area">
+              <a className="link">
+                area{' '}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="ml-1 h-5 w-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+                  />
+                </svg>
+              </a>
+            </Link>
+          </li>
+          <li>
             <Link href="/favourite">
               <a className="link">
                 favourites{' '}
