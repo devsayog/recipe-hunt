566fb38f567b4959717796516508a8d0d2909b49
devsayog <devsayog@gmail.com>
2022-09-17 18:12:10 +0545

    add favourite page

--- src/components/MealsList.tsx
+++ src/components/MealsList.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import type { Meal } from '@/types/meal'
 
 import MealCard from './MealCard'
-import { Heading2 } from './Typography'
+import { Heading2, Paragraph } from './Typography'
 
 interface IMealsListProps {
   meals: Meal[]
@@ -13,11 +13,18 @@ const MealsList = ({ meals, title }: IMealsListProps) => {
   return (
     <section>
       <Heading2>{title}</Heading2>
-      <div className="card-grid">
-        {meals.map((m) => (
-          <MealCard key={m.idMeal} meal={m} />
-        ))}
-      </div>
+      {meals.length > 0 ? (
+        <div className="card-grid">
+          {meals.map((m) => (
+            <MealCard key={m.idMeal} meal={m} />
+          ))}
+        </div>
+      ) : (
+        <Paragraph
+          className="mt-5 text-red-500 md:mt-8"
+          text={`No ${title} results found`}
+        />
+      )}
     </section>
   )
 }
---
+++ src/pages/favourite.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import MealsList from '@/components/MealsList'
+import { selectFav } from '@/slice/favourites'
+import { useAppSelector } from '@/store/useReduxHooks'
+
+const Favourites = () => {
+  const { favourites } = useAppSelector(selectFav)
+  return <MealsList meals={favourites} title="Favourites" />
+}
+
+export default Favourites
